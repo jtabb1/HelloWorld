720a389387cf5363d62391afe6225930283a1d85
Jeremiah Tabb <jeremiah.r29q7.tabb@gmail.com>
2019-09-26 16:21:23 -0400

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Matrix Has You...");
+            Console.ReadLine();
+        }
+    }
+}
